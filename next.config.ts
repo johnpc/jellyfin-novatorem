d4840534c297b5a2aae6b79fa3e342256c300f42
John Corser <johnpc@umich.edu>
2025-12-30 16:48:32 -0500

    Fix env vars for Amplify runtime

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    JELLYFIN_URL: process.env.JELLYFIN_URL,
+    JELLYFIN_API_KEY: process.env.JELLYFIN_API_KEY,
+    JELLYFIN_USERNAME: process.env.JELLYFIN_USERNAME,
+  },
 };
 
 export default nextConfig;
